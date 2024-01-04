2ce1b43134c9269d684a050e15a8533dceeebb0b
Gustavo Oliveira <guqoliveira@gmail.com>
2024-01-04 19:26:56 +0000

    CORS

--- web3-gaming-explorer-backend/src/index.ts
+++ web3-gaming-explorer-backend/src/index.ts
@@ -6,9 +6,11 @@ import { routes } from "./routes/index";
 dotenv.config();
 
 const app: Express = express();
+const cors = require("cors");
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/v1", routes);
 
